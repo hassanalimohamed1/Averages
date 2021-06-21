1c8bb2d3ab7ad385431fce1ca64947d653a81f2e
Hassan Mohamed <hassanalimohamed1@gmail.com>
2021-06-21 12:46:20 +1000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+namespace Averages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = { 7, 6, 5, 4, 3, 2 };
+            AvgCalc(23.56);
+            AvgCalc(44.23, 55.28, 61.78);
+            AvgCalc(26, 34, 84);
+            AvgCalc(array);
+        }
+        static void AvgCalc(params double[] ary) {
+            double total = 0;
+            double avg;
+
+            foreach (double number in ary) {
+                Write(number + " ");
+                total += number;
+            }
+            avg = total / ary.Length;
+            WriteLine($" -- Average is {avg}.");
+        }
+    }
+}
